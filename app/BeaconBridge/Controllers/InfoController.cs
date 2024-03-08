a0b3600f46418a84e5c672e9da8e4b32cab08fdb
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-03-08 15:17:44 +0000

    move code under one method and route

--- app/BeaconBridge/Controllers/InfoController.cs
+++ app/BeaconBridge/Controllers/InfoController.cs
@@ -1,6 +1,5 @@
 using BeaconBridge.Config;
 using BeaconBridge.Models;
-using BeaconBridge.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -9,14 +8,12 @@ namespace BeaconBridge.Controllers;
 [ApiController]
 [Route("api/")]
 public class InfoController(IOptions<BeaconInfoOptions> beaconInfoOptions,
-    IOptions<OrganisationOptions> organisationOptions,InfoService infoService)
+    IOptions<OrganisationOptions> organisationOptions)
   : ControllerBase
 {
   private readonly BeaconInfoOptions _beaconInfoOptions = beaconInfoOptions.Value;
   private readonly OrganisationOptions _organisationOptions = organisationOptions.Value;
-  private readonly InfoService _infoService = infoService;
-
-  [HttpGet,Route("")]
+  [HttpGet,Route(""),Route("info")]
   public ActionResult<Info> Get([FromQuery] string requestedSchema)
   {
     var info = new Info()
@@ -25,8 +22,7 @@ public class InfoController(IOptions<BeaconInfoOptions> beaconInfoOptions,
       {
         ApiVersion = _beaconInfoOptions.ApiVersion,
         BeaconId = _beaconInfoOptions.BeaconId,
-        Granularity = _beaconInfoOptions.Granularity,
-        RequestSummary = _infoService.SetSummary(requestedSchema)
+        Granularity = _beaconInfoOptions.Granularity
       },
       Response =
       {
@@ -39,34 +35,6 @@ public class InfoController(IOptions<BeaconInfoOptions> beaconInfoOptions,
         Version = _beaconInfoOptions.Version,
         WelcomeUrl = _beaconInfoOptions.WelcomeUrl,
         AlternativeUrl = _beaconInfoOptions.AlternativeUrl,
-      }
-    };
-    return info;
-  }
-  
-  [HttpGet,Route("[controller]")]
-  public ActionResult<Info> GetInfo([FromQuery] string requestedSchema)
-  {
-    var info = new Info()
-    {
-      Meta =
-      {
-        ApiVersion = _beaconInfoOptions.ApiVersion,
-        BeaconId = _beaconInfoOptions.BeaconId,
-        Granularity = _beaconInfoOptions.Granularity,
-        RequestSummary = _infoService.SetSummary(requestedSchema)
-      },
-      Response =
-      {
-        Id = _beaconInfoOptions.BeaconId,
-        Name = _beaconInfoOptions.Name,
-        ApiVersion = _beaconInfoOptions.ApiVersion,
-        Environment = _beaconInfoOptions.Environment,
-        Organisation = _organisationOptions,
-        Description = _beaconInfoOptions.Description,
-        Version = _beaconInfoOptions.Version,
-        WelcomeUrl = _beaconInfoOptions.WelcomeUrl,
-        AlternativeUrl = _beaconInfoOptions.AlternativeUrl
       }
     };
     return info;
