22e87833e1e255366511960fd55ba44d1386a947
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-03-08 15:18:22 +0000

    Set RequestSummary.cs defaults

--- app/BeaconBridge/Models/RequestSummary.cs
+++ app/BeaconBridge/Models/RequestSummary.cs
@@ -8,9 +8,9 @@ public class RequestSummary
   public string Schemas { get; set; } = string.Empty;
   public List<string> Filters { get; set; } = new();
   public string Parameters { get; set; } = string.Empty;
-  public string IncludeResultSetResponses { get; set; } = string.Empty;
+  public string IncludeResultSetResponses { get; set; } = ResultsetResponses.Hit;
   public string Pagination { get; set; } = string.Empty;
-  public Granularity Granularity { get; set; }
+  public string Granularity { get; set; } = Constants.Granularity.Boolean;
   public bool TestMode { get; set; } = false;
 
 }
