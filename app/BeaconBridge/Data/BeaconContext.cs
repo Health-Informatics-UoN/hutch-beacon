ed8306656558f7f7d901c1f1d35da617c9c75605
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-11 15:29:43 +0000

    add beacon db context

---
+++ app/BeaconBridge/Data/BeaconContext.cs
@@ -0,0 +1,28 @@
+using BL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeaconBridge.Data;
+
+public class BeaconContext : DbContext
+{
+  public BeaconContext(DbContextOptions<BeaconContext> options)
+    : base(options)
+  {
+           
+  }
+
+  public DbSet<User> Users { get; set; }
+  public DbSet<Project> Projects { get; set; }
+        
+        
+  public DbSet<BL.Models.Tre> Tres { get; set; }
+
+  public DbSet<Submission> Submissions { get; set; }
+  public DbSet<HistoricStatus> HistoricStatuses { get; set; }
+  public DbSet<AuditLog> AuditLogs { get; set; }
+  public DbSet<SubmissionFile> SubmissionFiles { get; set; }
+
+  public DbSet<ProjectTreDecision> ProjectTreDecisions { get; set; }
+
+  public DbSet<MembershipTreDecision> MembershipTreDecisions { get; set; }
+}
