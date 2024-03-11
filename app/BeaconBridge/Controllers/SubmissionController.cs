c7517af69fa5f58249f3683eebae30a5f946359f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-11 15:38:45 +0000

    primary constructor

--- app/BeaconBridge/Controllers/SubmissionController.cs
+++ app/BeaconBridge/Controllers/SubmissionController.cs
@@ -6,23 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace BeaconBridge.Controllers;
 
 [ApiController, Route("api/[controller]/")]
-public class SubmissionController : ControllerBase
+public class SubmissionController(BeaconContext db) : ControllerBase
 {
-  private readonly BeaconContext _db;
-  public SubmissionController(BeaconContext db)
-  {
-    _db = db;
-  }
-
   [HttpGet]
   [Route("GetWaitingSubmissionsForTre")]
   public async Task<IActionResult> GetWaitingSubmissionsForTre()
   {
     var usersName = (from x in User.Claims where x.Type == "preferred_username" select x.Value).First();
-    var tre = ControllerHelpers.GetUserTre(User, _db);
+    var tre = ControllerHelpers.GetUserTre(User, db);
 
     tre.LastHeartBeatReceived = DateTime.Now.ToUniversalTime();
-    await _db.SaveChangesAsync();
+    await db.SaveChangesAsync();
     var results = tre.Submissions.Where(x => x.Status == StatusType.WaitingForAgentToTransfer).ToList();
 
 
