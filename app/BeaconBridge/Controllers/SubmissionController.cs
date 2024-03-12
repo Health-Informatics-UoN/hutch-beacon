c94d465b9b63ffa81f30b8ae5cecb1e0e88d11c3
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-12 12:02:32 +0000

    GetRequestCancelSubsForTre

--- app/BeaconBridge/Controllers/SubmissionController.cs
+++ app/BeaconBridge/Controllers/SubmissionController.cs
@@ -22,6 +22,23 @@ public class SubmissionController(BeaconContext db, ControllerHelpers helpers) :
     await db.SaveChangesAsync();
     var results = tre.Submissions.Where(x => x.Status == StatusType.WaitingForAgentToTransfer).ToList();
 
+    return Ok(results);
+  }
+  
+  [HttpGet, Route("get-request-cancel-subs-for-tre")]
+  public async Task<IActionResult> GetRequestCancelSubsForTre()
+  {
+    var tre = await helpers.GetUserTre(User, db);
+    if (tre == null)
+    {
+      return NotFound();
+    }
+
+    tre.LastHeartBeatReceived = DateTime.Now.ToUniversalTime();
+    await db.SaveChangesAsync();
+    var results = tre.Submissions.Where(x => x.Status == StatusType.RequestCancellation).ToList();
+
+
     return StatusCode(200, results);
   }
 }
--- app/BeaconBridge/Startup/Web/ConfigureWebService.cs
+++ app/BeaconBridge/Startup/Web/ConfigureWebService.cs
@@ -4,11 +4,11 @@ public static class ConfigureWebService
 {
   public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder b)
   {
+    b.Services.AddRouting(options => options.LowercaseUrls = true);
     b.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     b.Services.AddEndpointsApiExplorer();
     b.Services.AddSwaggerGen();
-    b.Services.AddRouting(options => options.LowercaseUrls = true);
 
     // Add Options
 
