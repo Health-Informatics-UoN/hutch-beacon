518f387d79e4f81e0f490c1b918f0299f5952cea
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-03-19 14:36:15 +0000

    allow for parameters to be empty in response

--- app/BeaconBridge/Models/RequestSummary.cs
+++ app/BeaconBridge/Models/RequestSummary.cs
@@ -1,16 +1,32 @@
+using System.Text.Json.Serialization;
 using BeaconBridge.Constants;
 
 namespace BeaconBridge.Models;
 
 public class RequestSummary
 {
-  public string ApiVersion { get; set; } = string.Empty;
-  public string Schemas { get; set; } = string.Empty;
-  public List<string> Filters { get; set; } = new();
-  public string Parameters { get; set; } = string.Empty;
-  public string IncludeResultSetResponses { get; set; } = ResultsetResponses.Hit;
-  public string Pagination { get; set; } = string.Empty;
-  public string Granularity { get; set; } = Constants.Granularity.Boolean;
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]  
+  public string? ApiVersion { get; set; }
+  
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]  
+  public string? Schemas { get; set; }
+  
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]  
+  public List<string>? Filters { get; set; }
+  
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]  
+  public string? Parameters { get; set; }
+  
+  public string? IncludeResultSetResponses { get; set; } = ResultsetResponses.Hit;
+
+  public Dictionary<string, int>? Pagination { get; set; } = new()
+  {
+    { "skip", 0 },
+    { "limit", 10 }
+  };
+  
+  public string? Granularity { get; set; } = Constants.Granularity.Boolean;
+  
   public bool TestMode { get; set; } = false;
 
 }
