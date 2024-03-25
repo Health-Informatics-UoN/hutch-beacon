dad9378e6014cf60a5558d20df4b9eef8738ef7d
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-25 15:32:11 +0000

    Submission.cs

---
+++ app/BeaconBridge/Data/Entities/Submission.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeaconBridge.Data.Entities;
+
+public class Submission
+{
+  public int Id { get; set; }
+  public int? ParentId { get; set; }
+  public string? TesId { get; set; }
+  public string SourceCrate { get; set; }
+  public string TesName { get; set; }
+  public string? TesJson { get; set; }
+
+  public string? FinalOutputFile { get; set; }
+  public string DockerInputLocation { get; set; }
+  public Project Project { get; set; }
+  [ForeignKey("ParentID")]
+  public Submission? Parent { get; set; }
+  public List<Submission> Children { get; set; }
+  public List<HistoricStatus> HistoricStatuses { get; set; }
+  [NotMapped]
+  public List<StageInfo> StageInfo { get; set; }
+  public List<SubmissionFile> SubmissionFiles { get; set; }
+  public List<AuditLog>? AuditLogs { get; set; }
+  public Tre? Tre { get; set; }
+  public User SubmittedBy { get; set; }
+  public DateTime LastStatusUpdate { get; set; }
+  public DateTime StartTime { get; set; }
+  public DateTime EndTime { get; set; }
+  public StatusType Status { get; set; }
+  public string? StatusDescription { get; set; }
+}
