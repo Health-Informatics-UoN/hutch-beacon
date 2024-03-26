d48eb0d24235f9d92f51a33c011a82d135c16f00
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-26 11:49:33 +0000

    ProjectTreDecision.cs and Decision.cs

---
+++ app/BeaconBridge/Data/Entities/ProjectTreDecision.cs
@@ -0,0 +1,11 @@
+using BeaconBridge.Constants.Submission;
+
+namespace BeaconBridge.Data.Entities;
+
+public class ProjectTreDecision
+{
+  public int Id { get; set; }
+  public Project? SubmissionProj { get; set; }
+  public Tre? Tre { get; set; }
+  public Decision Decision { get; set; }
+}
