9b356dfcf56477807608207ef0639cfc91eb2017
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-26 11:58:14 +0000

    db context and entity framework

--- app/BeaconBridge/Data/DbContext.cs
+++ app/BeaconBridge/Data/DbContext.cs
@@ -1,6 +1,17 @@
+using BeaconBridge.Data.Entities.Submission;
+using Microsoft.EntityFrameworkCore;
+
 namespace BeaconBridge.Data;
 
 public class DbContext
 {
-  
+  public DbSet<User> Users { get; set; }
+  public DbSet<Project> Projects { get; set; }
+  public DbSet<Tre> Tres { get; set; }
+  public DbSet<Submission> Submissions { get; set; }
+  public DbSet<HistoricStatus> HistoricStatuses { get; set; }
+  public DbSet<AuditLog> AuditLogs { get; set; }
+  public DbSet<SubmissionFile> SubmissionFiles { get; set; }
+  public DbSet<ProjectTreDecision> ProjectTreDecisions { get; set; }
+  public DbSet<MembershipTreDecision> MembershipTreDecisions { get; set; }
 }
