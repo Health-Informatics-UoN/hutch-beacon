629b533708a2d996a452051338c2925982c20c76
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-26 11:07:15 +0000

    user class

---
+++ app/BeaconBridge/Data/Entities/User.cs
@@ -0,0 +1,17 @@
+namespace BeaconBridge.Data.Entities;
+
+public class User
+{
+  public int Id { get; set; }
+  public string? FullName { get; set; }   
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public List<Project> Projects { get; set; }
+  public List<Submission> Submissions { get; set; }
+  public string FormData { get; set; }
+
+  public string? Biography { get; set; }
+  public string? Organisation {get;set;}
+  public List<MembershipTreDecision> MembershipTreDecision { get; set; }
+  public List<AuditLog>? AuditLogs { get; set; }
+}
