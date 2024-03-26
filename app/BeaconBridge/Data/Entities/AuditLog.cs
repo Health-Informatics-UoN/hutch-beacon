f20e15bdf5b91c703a52e87bc734192a0cab03b7
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-03-26 11:19:04 +0000

    audit log

---
+++ app/BeaconBridge/Data/Entities/AuditLog.cs
@@ -0,0 +1,17 @@
+using BeaconBridge.Constants.Submission;
+
+namespace BeaconBridge.Data.Entities;
+
+public class AuditLog
+{
+  public int Id { get; set; }
+  public Project? Project { get; set; }
+  public User? User { get; set; }
+  public Tre? Tre { get; set; }
+  public Submission? Submission { get; set; }
+  public string? LoggedInUserName { get; set; }
+  public string? HistoricFormData { get; set; }
+  public string? IPaddress { get; set; }
+  public LogType LogType { get; set; }
+  public DateTime Date { get; set; }
+}
