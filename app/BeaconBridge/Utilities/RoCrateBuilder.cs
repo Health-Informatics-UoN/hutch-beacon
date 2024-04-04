9f46408e2c2f46a09e4cd619b31448df0525cf41
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-04 13:50:12 +0100

    crate resetting and returning

--- app/BeaconBridge/Utilities/RoCrateBuilder.cs
+++ app/BeaconBridge/Utilities/RoCrateBuilder.cs
@@ -12,4 +12,23 @@ public class RoCrateBuilder
   private readonly CratePublishingOptions _publishingOptions;
   private readonly WorkflowOptions _workflowOptions;
   private ROCrate _crate = new();
+
+  /// <summary>
+  /// Returns the <c>ROCrate</c> that has been built.
+  /// </summary>
+  /// <returns>The <c>ROCrate</c> that has been built.</returns>
+  public ROCrate GetROCrate()
+  {
+    var result = _crate;
+    ResetCrate();
+    return result;
+  }
+
+  /// <summary>
+  /// Resets the <c>ROCrate</c> object in the builder.
+  /// </summary>
+  private void ResetCrate()
+  {
+    _crate = new ROCrate();
+  }
 }
