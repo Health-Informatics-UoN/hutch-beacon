c02f0ae1c129be149727422ae54c37670ea8b835
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-04 16:56:24 +0100

    Add FiveSafes.Net from `RquestBridge` (#39)
    * initial move FiveSafes.Net
    
    * add FiveSafes.Net.Tests to sln
    
    * add FiveSafes.Net to sln
    
    * fix missing imports
    
    * upgrade to dotnet 8

---
+++ lib/FiveSafes.Net/Constants/BagItConstants.cs
@@ -0,0 +1,10 @@
+namespace FiveSafes.Net.Constants;
+
+public class BagItConstants
+{
+  public const string ManifestPath = "manifest-sha512.txt";
+  public const string TagManifestPath = "tagmanifest-sha512.txt";
+  public const string BagitTxtPath = "bagit.txt";
+  public const string BagInfoTxtPath = "bag-info.txt";
+  public const string PayloadDirectory = "data";
+}
