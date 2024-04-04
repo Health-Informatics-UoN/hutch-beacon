a4f37d15085eb539ab3748feb926034bc66ec249
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-04 13:57:30 +0100

    crate builder constructors

--- app/BeaconBridge/Utilities/RoCrateBuilder.cs
+++ app/BeaconBridge/Utilities/RoCrateBuilder.cs
@@ -13,6 +13,38 @@ public class RoCrateBuilder
   private readonly WorkflowOptions _workflowOptions;
   private ROCrate _crate = new();
 
+  public RoCrateBuilder(WorkflowOptions workflowOptions, CratePublishingOptions publishingOptions,
+    CrateAgentOptions crateAgentOptions, CrateProjectOptions crateProjectOptions,
+    CrateOrganizationOptions crateOrganizationOptions,
+    string archivePayloadDirectoryPath, AgreementPolicyOptions agreementPolicy)
+  {
+    _workflowOptions = workflowOptions;
+    _publishingOptions = publishingOptions;
+    _crateAgentOptions = crateAgentOptions;
+    _crateProjectOptions = crateProjectOptions;
+    _crateOrganizationOptions = crateOrganizationOptions;
+    _agreementPolicy = agreementPolicy;
+
+    _crate.Initialise(archivePayloadDirectoryPath);
+    AddProject();
+    AddOrganisation();
+  }
+
+  public RoCrateBuilder(WorkflowOptions workflowOptions, CratePublishingOptions publishingOptions,
+    CrateAgentOptions crateAgentOptions, CrateProjectOptions crateProjectOptions,
+    CrateOrganizationOptions crateOrganizationOptions, AgreementPolicyOptions agreementPolicy)
+  {
+    _workflowOptions = workflowOptions;
+    _publishingOptions = publishingOptions;
+    _crateAgentOptions = crateAgentOptions;
+    _crateProjectOptions = crateProjectOptions;
+    _crateOrganizationOptions = crateOrganizationOptions;
+    _agreementPolicy = agreementPolicy;
+
+    AddProject();
+    AddOrganisation();
+  }
+
   /// <summary>
   /// Returns the <c>ROCrate</c> that has been built.
   /// </summary>
