38385159b8403dfb229f93eca241890cc872041f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-04 10:23:02 +0100

    initial move FiveSafes.Net

---
+++ lib/FiveSafes.Net/FiveSafes.Net/Constants/FiveSafesProfile.cs
@@ -0,0 +1,8 @@
+namespace FiveSafes.Net.Constants;
+
+public class FiveSafesProfile
+{
+  public string Id { get; set; } = "https://w3id.org/5s-crate/0.4";
+  public string Type { get; set; } = "Profile";
+  public string Name { get; set; } = "Trusted Workflow Run Crate profile";
+}
