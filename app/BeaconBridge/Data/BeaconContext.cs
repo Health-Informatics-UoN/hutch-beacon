ea3e8af516b693294549970976f3dc890ac3e1ac
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-05 14:07:37 +0100

    add filtering terms set and correct type annotation

--- app/BeaconBridge/Data/BeaconContext.cs
+++ app/BeaconBridge/Data/BeaconContext.cs
@@ -1,14 +1,17 @@
+using BeaconBridge.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeaconBridge.Data;
 
 public class BeaconContext : DbContext
 {
-  public BeaconContext(DbContextOptions<SubmissionContext> options) : base(options)
+  public BeaconContext(DbContextOptions<BeaconContext> options) : base(options)
   {
   }
 
   public BeaconContext()
   {
   }
+
+  public DbSet<FilteringTerm> FilteringTerms => Set<FilteringTerm>();
 }
