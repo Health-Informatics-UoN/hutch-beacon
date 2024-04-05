05ca491b4a63a487165396d00c044e336f8375ef
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-05 14:17:35 +0100

    Implement working startup pattern for migrations

--- app/BeaconBridge/Program.cs
+++ app/BeaconBridge/Program.cs
@@ -2,7 +2,11 @@ using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
 using BeaconBridge.Commands.Helpers;
 using BeaconBridge.Startup.Cli;
+using BeaconBridge.Startup.EfCoreMigrations;
 using BeaconBridge.Startup.Web;
+
+// Enable EF Core tooling to get a DbContext configuration
+EfCoreMigrations.BootstrapDbContext(args);
 
 await new CommandLineBuilder(new CliEntrypoint())
   .UseDefaults()
