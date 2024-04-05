e8fecd4ea55821da64f27082370e8ec35cd98a08
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-05 14:48:38 +0100

    fix userhelper error

--- app/BeaconBridge/Startup/Web/ConfigureWebService.cs
+++ app/BeaconBridge/Startup/Web/ConfigureWebService.cs
@@ -13,11 +13,6 @@ public static class ConfigureWebService
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     b.Services.AddEndpointsApiExplorer();
     b.Services.AddSwaggerGen();
-    // b.Services.AddDbContext<BeaconContext>(o =>
-    // {
-    //   var connectionString = b.Configuration.GetConnectionString("BeaconBridgeDb");
-    //   o.UseSqlite(connectionString ?? "Data Source=BeaconBridge.db");
-    // });
     b.Services
       .AddAutoMapper(typeof(Program).Assembly)
       .AddSingleton<IFlurlClientFactory, PerBaseUrlFlurlClientFactory>()
@@ -41,7 +36,7 @@ public static class ConfigureWebService
 
     // Add Services
     b.Services
-      .AddTransient<UserHelper>()
+      // .AddTransient<UserHelper>()  // Not used at the moment
       .AddTransient<OpenIdIdentityService>()
       .AddTransient<MinioService>()
       .AddTransient<CrateGenerationService>()
