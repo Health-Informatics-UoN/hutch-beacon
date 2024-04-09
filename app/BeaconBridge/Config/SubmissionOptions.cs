a7f5366e9299a5f5c3aff940e34ef35f64b25b01
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-09 16:33:56 +0100

    add Tres to SubmissionOptions.cs

--- app/BeaconBridge/Config/SubmissionOptions.cs
+++ app/BeaconBridge/Config/SubmissionOptions.cs
@@ -6,4 +6,9 @@ public class SubmissionOptions
   /// The name of the project to submit requests against on the Submission Layer.
   /// </summary>
   public string ProjectName { get; set; } = string.Empty;
+
+  /// <summary>
+  /// The list of TREs to submit the requests against on the Submission Layer.
+  /// </summary>
+  public List<string> Tres { get; set; } = new();
 }
