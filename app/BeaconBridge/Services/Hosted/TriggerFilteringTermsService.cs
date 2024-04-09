e4968bf6259cc7e7614be1965b312b988d57233f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-09 13:46:36 +0100

    check minio connection works

--- app/BeaconBridge/Services/Hosted/TriggerFilteringTermsService.cs
+++ app/BeaconBridge/Services/Hosted/TriggerFilteringTermsService.cs
@@ -7,10 +7,8 @@ namespace BeaconBridge.Services.Hosted;
 /// Hosted service for sending requests to the submission to trigger a workflow run that gets the filtering
 /// terms for an OMOP database.
 /// </summary>
-/// <param name="options"></param>
-/// <param name="logger"></param>
 public class TriggerFilteringTermsService(IOptions<FilteringTermsUpdateOptions> options,
-  ILogger<TriggerFilteringTermsService> logger) : BackgroundService
+  ILogger<TriggerFilteringTermsService> logger, MinioService minio) : BackgroundService
 {
   private readonly FilteringTermsUpdateOptions _options = options.Value;
 
@@ -20,6 +18,22 @@ public class TriggerFilteringTermsService(IOptions<FilteringTermsUpdateOptions> 
     while (!stoppingToken.IsCancellationRequested)
     {
       var delay = Task.Delay(TimeSpan.FromSeconds(_options.DelaySeconds), stoppingToken);
+
+      // Add the workflow crate to MinIO
+      if (await minio.StoreExists())
+      {
+        logger.LogInformation("Saving Filtering Terms workflow to object store");
+        // Todo: save workflow to Minio
+      }
+      else
+      {
+        logger.LogCritical("Cannot save Filtering Terms workflow. Object store does not exist");
+      }
+
+      // Build the TES task
+
+      // Submit to submission layer
+
       await delay;
     }
   }
