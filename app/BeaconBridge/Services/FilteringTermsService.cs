269f1b35f4743b603e7f2dc0c94bccac62ce6363
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-11 15:18:22 +0100

    add method to save filtering terms to the db

--- app/BeaconBridge/Services/FilteringTermsService.cs
+++ app/BeaconBridge/Services/FilteringTermsService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using BeaconBridge.Data;
+using BeaconBridge.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeaconBridge.Services;
@@ -12,5 +13,26 @@ public class FilteringTermsService(BeaconContext context, IMapper mapper)
       await context.FilteringTerms
         .AsNoTracking()
         .ToListAsync());
+  }
+
+  /// <summary>
+  /// Save a list of filtering terms to the DB cache.
+  /// </summary>
+  /// <param name="filteringTerms">The list of filtering terms.</param>
+  public async Task SaveRangeAsync(IEnumerable<Models.FilteringTerm> filteringTerms)
+  {
+    var termEntities = filteringTerms.Select(term => new FilteringTerm
+    {
+      Type = term.Type,
+      Id = term.Id,
+      Label = term.Label,
+      Scope = term.Scope
+    });
+    foreach (var entity in termEntities)
+    {
+      await context.AddAsync(entity);
+    }
+
+    await context.SaveChangesAsync();
   }
 }
