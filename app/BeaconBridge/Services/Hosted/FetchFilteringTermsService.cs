6dbc60ecc81797a360e297ebb724b3ac9b36325d
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-17 11:39:55 +0100

    more logging and error catching

--- app/BeaconBridge/Services/Hosted/FetchFilteringTermsService.cs
+++ app/BeaconBridge/Services/Hosted/FetchFilteringTermsService.cs
@@ -2,6 +2,7 @@ using System.IO.Compression;
 using System.Text.Json;
 using BeaconBridge.Config;
 using Microsoft.Extensions.Options;
+using Minio.Exceptions;
 
 namespace BeaconBridge.Services.Hosted;
 
@@ -27,27 +28,37 @@ public class FetchFilteringTermsService(IOptions<FilteringTermsUpdateOptions> fi
         {
           logger.LogInformation("Downloading the results RO-Crate of the workflow");
           await minio.GetFromStore(objectNameInBucket, filteringTermsOptions.Value.PathToResults);
+          logger.LogInformation("Successfully downloaded the results RO-Crate of the workflow");
         }
-        catch (Exception)
+        catch (BucketNotFoundException)
         {
-          logger.LogCritical("Unable to download results RO-Crate from the store");
+          logger.LogError("Unable to download results RO-Crate from the store");
           continue;
         }
       }
       else
       {
-        logger.LogWarning("Minio bucket does not exist or results RO-Crate {Object} not in store", objectNameInBucket);
+        logger.LogError("Minio bucket does not exist or results RO-Crate {Object} not in store", objectNameInBucket);
+        continue;
       }
 
       // Unzip RO-Crate
       if (objectDirectoryOnDisk is null)
       {
-        logger.LogCritical("Couldn't locate download path for results RO-Crate on disk");
+        logger.LogError("Couldn't locate download path for results RO-Crate on disk");
         throw new DirectoryNotFoundException($"{objectDirectoryOnDisk} not found");
       }
 
       var dirInfo = new DirectoryInfo(objectDirectoryOnDisk);
-      ZipFile.ExtractToDirectory(filteringTermsOptions.Value.PathToResults, dirInfo.Parent!.FullName);
+      try
+      {
+        ZipFile.ExtractToDirectory(filteringTermsOptions.Value.PathToResults, dirInfo.Parent!.FullName);
+      }
+      catch (Exception e) when (e is NullReferenceException or IOException)
+      {
+        logger.LogError("Unable to unzip results RO-Crate");
+        continue;
+      }
 
       try
       {
@@ -61,16 +72,21 @@ public class FetchFilteringTermsService(IOptions<FilteringTermsUpdateOptions> fi
         // Save results to the cache
         await using var scope = serviceProvider.CreateAsyncScope();
         var filteringTermsService = scope.ServiceProvider.GetRequiredService<FilteringTermsService>();
-        await filteringTermsService.SaveRangeAsync(filteringTerms ?? throw new InvalidOperationException());
+        await filteringTermsService.AddOrUpdateRangeAsync(filteringTerms ?? throw new InvalidOperationException());
       }
       catch (InvalidOperationException)
       {
-        logger.LogCritical("Unable to locate filtering terms RO-Crate or results file");
+        logger.LogError("Unable to locate filtering terms RO-Crate or results file");
         continue;
       }
       catch (Exception e) when (e is NullReferenceException or JsonException)
       {
-        logger.LogCritical("Unable to deserialise filtering terms file");
+        logger.LogError("Unable to deserialise filtering terms file");
+        continue;
+      }
+      catch (OperationCanceledException)
+      {
+        logger.LogError("Unable to read filtering terms JSON file");
         continue;
       }
       finally
