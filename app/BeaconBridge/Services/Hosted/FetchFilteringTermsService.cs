a80b51c98365fc3806c0193520931ac4eda28056
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-04-17 11:11:54 +0100

    debug integration

--- app/BeaconBridge/Services/Hosted/FetchFilteringTermsService.cs
+++ app/BeaconBridge/Services/Hosted/FetchFilteringTermsService.cs
@@ -7,23 +7,26 @@ namespace BeaconBridge.Services.Hosted;
 
 public class FetchFilteringTermsService(IOptions<FilteringTermsUpdateOptions> filteringTermsOptions,
   ILogger<FetchFilteringTermsService> logger, MinioService minio,
-  FilteringTermsService filteringTermsService) : BackgroundService
+  IServiceProvider serviceProvider) : BackgroundService
 {
   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
   {
     logger.LogInformation("Triggering filtering terms cache update");
-    var objectName = Path.GetFileName(filteringTermsOptions.Value.PathToResults);
+    var objectNameInBucket = Path.GetFileName(filteringTermsOptions.Value.PathToResults);
+    var objectDirectoryOnDisk = filteringTermsOptions.Value.PathToResults.Replace(
+      Path.GetExtension(objectNameInBucket), "");
+
     while (!stoppingToken.IsCancellationRequested)
     {
       var delay = Task.Delay(TimeSpan.FromSeconds(filteringTermsOptions.Value.DelaySeconds), stoppingToken);
 
       // Download RO-Crate from MinIO
-      if (await minio.StoreExists() && minio.ObjectIsInStore(objectName))
+      if (await minio.StoreExists() && minio.ObjectIsInStore(objectNameInBucket))
       {
         try
         {
           logger.LogInformation("Downloading the results RO-Crate of the workflow");
-          await minio.GetFromStore(objectName, filteringTermsOptions.Value.PathToResults);
+          await minio.GetFromStore(objectNameInBucket, filteringTermsOptions.Value.PathToResults);
         }
         catch (Exception)
         {
@@ -31,21 +34,33 @@ public class FetchFilteringTermsService(IOptions<FilteringTermsUpdateOptions> fi
           continue;
         }
       }
+      else
+      {
+        logger.LogWarning("Minio bucket does not exist or results RO-Crate {Object} not in store", objectNameInBucket);
+      }
 
       // Unzip RO-Crate
-      var dirInfo = new DirectoryInfo(Path.GetDirectoryName(filteringTermsOptions.Value.PathToResults)!);
-      ZipFile.ExtractToDirectory(filteringTermsOptions.Value.PathToResults, dirInfo.FullName);
+      if (objectDirectoryOnDisk is null)
+      {
+        logger.LogCritical("Couldn't locate download path for results RO-Crate on disk");
+        throw new DirectoryNotFoundException($"{objectDirectoryOnDisk} not found");
+      }
+
+      var dirInfo = new DirectoryInfo(objectDirectoryOnDisk);
+      ZipFile.ExtractToDirectory(filteringTermsOptions.Value.PathToResults, dirInfo.Parent!.FullName);
 
       try
       {
         // Get the file containing the results
-        var file = dirInfo.EnumerateFiles("data/outputs/*/output.json").First();
+        var file = dirInfo.EnumerateFiles("output.json", SearchOption.AllDirectories).First();
 
         // Deserialise the file
         var termsJson = await File.ReadAllTextAsync(file.FullName, stoppingToken);
         var filteringTerms = JsonSerializer.Deserialize<List<Models.FilteringTerm>>(termsJson);
 
         // Save results to the cache
+        await using var scope = serviceProvider.CreateAsyncScope();
+        var filteringTermsService = scope.ServiceProvider.GetRequiredService<FilteringTermsService>();
         await filteringTermsService.SaveRangeAsync(filteringTerms ?? throw new InvalidOperationException());
       }
       catch (InvalidOperationException)
@@ -53,10 +68,14 @@ public class FetchFilteringTermsService(IOptions<FilteringTermsUpdateOptions> fi
         logger.LogCritical("Unable to locate filtering terms RO-Crate or results file");
         continue;
       }
-      catch (NullReferenceException)
+      catch (Exception e) when (e is NullReferenceException or JsonException)
       {
         logger.LogCritical("Unable to deserialise filtering terms file");
         continue;
+      }
+      finally
+      {
+        if (dirInfo.Exists) dirInfo.Delete(recursive: true);
       }
 
       await delay;
