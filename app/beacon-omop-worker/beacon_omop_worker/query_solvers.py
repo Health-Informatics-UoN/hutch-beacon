97cb15ed8aa3172cb0d2b4f54c11af65ceaedb4a
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-04-25 10:00:08 +0100

    add docstrings to query_solvers.py

--- app/beacon-omop-worker/beacon_omop_worker/query_solvers.py
+++ app/beacon-omop-worker/beacon_omop_worker/query_solvers.py
@@ -59,7 +59,12 @@ class FilterQuerySolver:
     def __init__(self, db_manager: SyncDBManager) -> None:
         self.db_manager = db_manager
 
-    def _get_concepts(self):
+    def _get_concepts(self) -> pd.DataFrame:
+        """
+        Select vocabulary_id,concept_code, concept_name, concept_id columns from Concept table
+        Returns:
+        concepts_df (pd.DataFrame): Concept table with relevant columns as a pandas dataframe.
+        """
         concept_query = select(
             Concept.vocabulary_id,
             Concept.concept_code,
@@ -73,14 +78,32 @@ class FilterQuerySolver:
         return concepts_df
 
     def _get_table_concepts(self, query) -> pd.DataFrame:
+        """
+        Given a SQL query execute it and return the results in a pandas dataframe.
+        Args:
+            query (str): A SQL query to execute
 
+        Returns:
+            table_concepts_df (pd.DataFrame): A pandas dataframe containing the results of the query.
+        """
         table_concepts_df = pd.read_sql_query(
             query, con=self.db_manager.engine.connect()
         )
         return table_concepts_df
 
     @staticmethod
-    def _group_person_concepts(concepts, person_concepts):
+    def _group_person_concepts(
+        concepts: pd.DataFrame, person_concepts: pd.DataFrame
+    ) -> List[FilteringTerm]():
+        """
+        Merge concepts dataframe with person dataframe on "race_concept_id" and "gender_concept_id".
+        Args:
+            concepts (pd.DataFrame): Dataframe containing all concept_ids
+            person_concepts (pd.DataFrame): Dataframe containing all concept_ids in the Person table
+
+        Returns:
+        filters (List[FilteringTerm]) : A list of filtering terms.
+        """
         gender_df = (
             concepts.merge(
                 person_concepts,
@@ -124,7 +147,18 @@ class FilterQuerySolver:
     def _group_filters(
         concepts: pd.DataFrame, omop_table_df: pd.DataFrame, column: str
     ) -> List[FilteringTerm]:
+        """
+        Merge two given dataframes on the concept_id column.
+        Create a list of the resulting filteringTerm objects.
+        Args:
+            concepts (pd.DataFrame): Dataframe containing all the concept ids.
+            omop_table_df (pd.DataFrame): Dataframe containing all the concept_ids in a specific table.
+            column (str): Name of the column to merge on.
 
+        Returns:
+            filters (List[FilteringTerm]) : A list of filtering terms.
+
+        """
         filters = list()
         filters_df = concepts.merge(
             omop_table_df,
@@ -143,7 +177,12 @@ class FilterQuerySolver:
         return filters
 
     def solve_concept_filters(self) -> List[FilteringTerm]:
-
+        """
+        For each OMOP table create SQL queries, build dataframe containing all concepts,group by concept_id
+        and append them to a list of FilteringTerm objects
+        Returns:
+            final_filters (List[FilteringTerm]) : A list of filtering terms.
+        """
         concepts = self._get_concepts()
 
         person_query = select(
@@ -187,6 +226,14 @@ class FilterQuerySolver:
 
 
 def solve_filters(db_manager: SyncDBManager):
+    """
+    Extract beacon filteringTerms from OMOP db.
+    Args:
+        db_manager (SyncDBManager): The database manager
+
+    Returns:
+     filters (List[FilteringTerm]) : A list of filtering terms
+    """
     logger = logging.getLogger(config.LOGGER_NAME)
     solver = FilterQuerySolver(db_manager=db_manager)
     try:
