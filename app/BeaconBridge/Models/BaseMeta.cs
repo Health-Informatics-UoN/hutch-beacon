def26dc060d272b39547296c24d636d4da875f0c
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-04-29 11:28:38 +0100

    Update Meta and BaseMeta for entrytypes

--- app/BeaconBridge/Models/BaseMeta.cs
+++ app/BeaconBridge/Models/BaseMeta.cs
@@ -1,15 +1,12 @@
-using System.Text.Json.Serialization;
+
 
 namespace BeaconBridge.Models;
 
 public class BaseMeta
 {
-  [JsonPropertyName("beaconId")] 
   public string BeaconId { get; set; } = string.Empty;
   
-  [JsonPropertyName("apiVersion")] 
   public string ApiVersion { get; set; } = string.Empty;
-
-  [JsonPropertyName("returnedSchemas")] 
-  public List<string> ReturnedSchemas { get; set; } = new();
+  
+  public List<Dictionary<string,string>> ReturnedSchemas { get; set; } = new();
 }
