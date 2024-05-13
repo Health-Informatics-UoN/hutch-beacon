bd1aa0d0f4f5ba58312695cea8be643267265750
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-13 13:25:35 +0100

    dummy text

--- app/beacon-bridge-ui/src/app/about/page.js
+++ app/beacon-bridge-ui/src/app/about/page.js
@@ -2,6 +2,13 @@ import Header from "@/app/components/Header";
 
 export default function About() {
   return(
-    <Header />
+    <div>
+      <Header />
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum commodo vel nibh at varius. Nam mattis enim ut massa auctor sodales. Nullam viverra dui aliquam purus laoreet, in tincidunt sapien lobortis. Aenean rhoncus neque quis volutpat hendrerit. Suspendisse potenti. Fusce pellentesque libero metus, ut semper metus rutrum non. In sit amet eros placerat, ullamcorper purus egestas, volutpat sem. Sed ac dictum urna, non interdum nibh. Suspendisse a congue orci. Aenean nec blandit nisl, at faucibus lacus. Vestibulum porta eu lectus non feugiat. Vivamus congue libero sed rhoncus porttitor.</p>
+        <p>Quisque a tincidunt lorem. Nullam justo est, scelerisque a consequat at, aliquet et mauris. Donec quis odio turpis. Praesent ut tellus a arcu rhoncus eleifend. Aenean at ipsum non neque sollicitudin accumsan non eget tortor. Sed porta sem in rhoncus ultricies. Quisque mi nisl, accumsan vel metus eu, tincidunt rutrum ligula. Aliquam luctus ligula nec fermentum luctus. Pellentesque vestibulum quam id laoreet scelerisque. In vel lectus suscipit, semper metus in, volutpat felis. Nulla euismod erat in tincidunt accumsan. Suspendisse pellentesque lorem porttitor, volutpat lectus a, efficitur dui. Quisque aliquam mi arcu, nec feugiat nulla facilisis ac.</p>
+      </div>
+    </div>
   )
 }
