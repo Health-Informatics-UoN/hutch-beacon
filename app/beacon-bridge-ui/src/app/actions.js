ed576a367468092418d9f2184c2041b95e42b4ea
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-14 13:16:06 +0100

    docstring

--- app/beacon-bridge-ui/src/app/actions.js
+++ app/beacon-bridge-ui/src/app/actions.js
@@ -2,6 +2,10 @@
 
 import { request } from "@/lib/api";
 
+/**
+ * Get the list of filtering terms from the Beacon.
+ * @returns The filtering terms.
+ */
 export async function getFilteringTerms() {
   var filteringTermsResponse = await request("filtering_terms")
   return filteringTermsResponse["response"]
