5367ba64ea7cbf392c55d68b013c4d55782347e4
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-14 10:48:32 +0100

    selected option component

---
+++ app/beacon-bridge-ui/src/app/components/SelectedOption.js
@@ -0,0 +1,14 @@
+import Button from "./Button"
+import { FcFullTrash } from "react-icons/fc"
+
+export default function SelectedOption({option, removeFunction}) {
+  const deleteIcon = new FcFullTrash()
+  return (
+    <div className="space-x-2 border-white border-2">
+      <span className={"flex items-center space-x-1"}>
+        <p className="grow">{`${option.id} - ${option.label}`}</p>
+        <Button icon={deleteIcon} text={"Remove"} onClick={() => removeFunction(option)}/>
+      </span>
+    </div>
+  )
+}
