a436e752876239f7269e2b735c169f430c68500b
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-14 10:48:51 +0100

    add onClick to Button

--- app/beacon-bridge-ui/src/app/components/Button.js
+++ app/beacon-bridge-ui/src/app/components/Button.js
@@ -1,6 +1,6 @@
-export default function Button({icon, text}) {
+export default function Button({icon, text, onClick}) {
   return (
-    <button className="w-24 bg-blue-950 px-2">
+    <button className="w-24 bg-blue-950 px-2" onClick={onClick}>
       <span className={"flex items-center space-x-1"}>
           {icon}
           <p className="grow">{text}</p>
