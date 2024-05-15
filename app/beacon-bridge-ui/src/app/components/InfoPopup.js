9ebf7073e47511d63e76bf313f8bafb0bed1e322
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 15:49:48 +0100

    customisable info popup

--- app/beacon-bridge-ui/src/app/components/InfoPopup.js
+++ app/beacon-bridge-ui/src/app/components/InfoPopup.js
@@ -1,14 +1,14 @@
 import { FcCheckmark } from "react-icons/fc";
 import { FcHighPriority } from "react-icons/fc";
 
-export default function InfoPopup({ text, isWarning }) {
+export default function InfoPopup({ text, isWarning, ...props }) {
   const tick = new FcCheckmark();
   const bang = new FcHighPriority();
 
-  var popupStyle = `rounded-lg border-2 border-white border-solid ${isWarning ? " bg-uon-red-20" : " bg-uon-bramley-20"}`;
+  var buttonColour = isWarning ? "bg-uon-red-20" : "bg-uon-bramley-20";
   var textColour = isWarning ? "text-uon-red-100" : "text-uon-bramley-100";
   return (
-    <div className={popupStyle}>
+    <div className={`${props.className} ${buttonColour} ${textColour}`}>
       <span className="flex items-center space-x-1">
         {isWarning ? bang : tick}
         <p className={textColour}>{text}</p>
