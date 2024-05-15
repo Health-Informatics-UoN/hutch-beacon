b340ed8b41f1d25d81c1b88514c7f3e0ce98a688
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 10:58:42 +0100

    style header

--- app/beacon-bridge-ui/src/app/components/Header.js
+++ app/beacon-bridge-ui/src/app/components/Header.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import Font from "next/font/google"
 
 export default function Header() {
   return (
     <div>
-      <span className={"flex items-center space-x-14"}>
+      <span className={"flex items-baseline space-x-14"}>
         <Image src={"/UoN-Nottingham-Blue-white-text-logo-RGB-300x112.png"} width={300} height={112} />
-        <h1 className={"text-xl"}>Beacon</h1>
+        <h1 className={"text-6xl"}>BEACON</h1>
       </span>
     </div>
   )
