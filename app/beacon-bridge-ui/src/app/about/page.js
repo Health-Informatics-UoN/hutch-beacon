94a9f87d15d98ae80ca265e5abbc61f83fb29b11
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 12:21:45 +0100

    padding on about page

--- app/beacon-bridge-ui/src/app/about/page.js
+++ app/beacon-bridge-ui/src/app/about/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Header from "@/app/components/Header";
 import Button from "@/app/components/Button";
 import SearchDropdown from "@/app/components/SearchDropdown";
 import SelectedOption from "@/app/components/SelectedOption";
@@ -60,19 +59,18 @@ export default function About() {
   }
 
   return(
-    <div>
-      <Header />
+    <div className="px-4">
       <div>
-        <h1>About</h1>
+        <h1 className="mb-4 text-xl md:text-2xl">About</h1>
         <p>This Beacon uses the GA4GH Beacon v2 API specification and is provided by the Centre for Health Informatics and Digital Research Service at the University of Nottingham.</p>
         <p>Beacon filters can be used to query a backend OMOP database of synthetic COVID-19 patient EHRs (electronic health records).</p>
         <p>The Beacon will return <span className="text-green-400">yes</span> or <span className="text-red-400">no</span> to indicate if the filters have been observed in any individuals.</p>
       </div>
-      <div>
-        <h1>Query</h1>
+      <div className="py-4">
+        <h1 className="text-xl md:text-2xl">Query</h1>
         <SearchDropdown
           id={"filtering-terms"}
-          label={"Filtering terms"}
+          label={"Select filtering terms"}
           options={filteringTerms}
           onChange={addSelection} />
         <Button icon={icon} text={"Search"} onClick={fetchIndividuals}/>
@@ -84,7 +82,7 @@ export default function About() {
           : "There are no individuals matching your query."}/>}
       </div>
       {selections.length > 0 &&
-        <div>
+        <div className="py-4">
           <h2>Selected</h2>
           {
             selections.map((s, key) => {
