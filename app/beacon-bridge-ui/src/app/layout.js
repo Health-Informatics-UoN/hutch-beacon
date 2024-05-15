454543e3978974a78e84248993de0ab0f11236fc
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 11:11:14 +0100

    fix fonts

--- app/beacon-bridge-ui/src/app/layout.js
+++ app/beacon-bridge-ui/src/app/layout.js
@@ -1,6 +1,8 @@
-import { inter } from "@/ui/fonts";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BEACON",
