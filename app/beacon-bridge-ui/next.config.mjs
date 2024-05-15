90e311f91ff1ae89c0863b276c148e3f6e347337
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 13:19:42 +0100

    standalone config

--- app/beacon-bridge-ui/next.config.mjs
+++ app/beacon-bridge-ui/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [{ key: "x-clacks-overhead", value: "GNU Terry Pratchett" }]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
