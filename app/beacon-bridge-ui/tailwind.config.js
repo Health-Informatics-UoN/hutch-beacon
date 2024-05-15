71a4eaa94c905aebe6a0f642b9242a8360da0abd
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 12:28:19 +0100

    configure text colours

--- app/beacon-bridge-ui/tailwind.config.js
+++ app/beacon-bridge-ui/tailwind.config.js
@@ -13,6 +13,36 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      // must have `transparent` and `current`
+      transparent: 'transparent',
+      current: 'currentColor',
+      // UoN primary colour https://www.nottingham.ac.uk/brand/visual/colour.aspx#Primarycolour
+      "uon-blue": {
+        100: "#10263B",
+        80: "#405162",
+        60: "#707D89",
+        40: "#9FA8B1",
+        20: "#CFD4D8",
+        5: "#F3F4F5"
+      },
+      "uon-bramley": {
+        100: "#93D500",
+        80: "#A9DD33",
+        60: "#BEE666",
+        40: "#D4EE99",
+        20: "#E9F7CC",
+        5: "#FAFDF2"
+      },
+      "uon-red": {
+        100: "#B91C2E",
+        80: "#C74958",
+        60: "#D57782",
+        40: "#E3A4AB",
+        20: "#F1D2D5",
+        5: "#FCF4F5"
+      }
+    }
   },
   plugins: [],
 };
