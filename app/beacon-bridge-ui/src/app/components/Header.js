eb76ca08bfbb062ef6f8ee4870885b04bc97d670
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 13:00:17 +0100

    swanky gradient

--- app/beacon-bridge-ui/src/app/components/Header.js
+++ app/beacon-bridge-ui/src/app/components/Header.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div>
+    <div className="bg-uon-gradient">
       <span className={"flex items-baseline space-x-14"}>
         <Image src={"/UoN-Nottingham-Blue-white-text-logo-RGB-300x112.png"} width={300} height={112} />
         <h1 className={"text-6xl"}>BEACON</h1>
