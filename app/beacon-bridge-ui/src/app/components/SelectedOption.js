e6591a4f2720ab17abf97e292842f73426c7f7fc
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 15:25:46 +0100

    style and format SelectedOption

--- app/beacon-bridge-ui/src/app/components/SelectedOption.js
+++ app/beacon-bridge-ui/src/app/components/SelectedOption.js
@@ -1,14 +1,19 @@
-import Button from "./Button"
-import { FcFullTrash } from "react-icons/fc"
+import Button from "./Button";
+import { FcFullTrash } from "react-icons/fc";
 
-export default function SelectedOption({option, removeFunction}) {
-  const deleteIcon = new FcFullTrash()
+export default function SelectedOption({ option, removeFunction }) {
+  const deleteIcon = new FcFullTrash();
   return (
-    <div className="space-x-2 border-uon-primary-80 border-2">
+    <div className="space-x-2 border-uon-blue-60 border-2 rounded-lg my-2">
       <span className={"flex items-center space-x-1"}>
         <p className="grow">{`${option.id} - ${option.label}`}</p>
-        <Button icon={deleteIcon} text={"Remove"} onClick={() => removeFunction(option)}/>
+        <Button
+          icon={deleteIcon}
+          text={"Remove"}
+          onClick={() => removeFunction(option)}
+          className="w-24 bg-uon-red-100 px-2 py-2 rounded-lg"
+        />
       </span>
     </div>
-  )
+  );
 }
