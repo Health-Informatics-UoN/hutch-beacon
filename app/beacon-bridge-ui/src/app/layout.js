2036ba0667396171c6bff0559bee9c18325fd698
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-05-15 13:00:31 +0100

    formating

--- app/beacon-bridge-ui/src/app/layout.js
+++ app/beacon-bridge-ui/src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
