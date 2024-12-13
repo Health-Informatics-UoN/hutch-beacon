879b01129f7d7c532dbeee6c648ccfbe170784cc
vpnu <vasiliki.panagi@nottingham.ac.uk>
2024-12-13 11:42:24 +0000

    Add UseRoCrate feature flag

--- app/BeaconBridge/Constants/FeatureFlags.cs
+++ app/BeaconBridge/Constants/FeatureFlags.cs
@@ -5,4 +5,6 @@ public class FeatureFlags
   public const string MakeAssessActions = nameof(MakeAssessActions);
 
   public const string ApproveEgress = nameof(ApproveEgress);
+
+  public const string UseRoCrate = nameof(UseRoCrate);
 }
