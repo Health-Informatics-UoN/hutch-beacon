0212864ffb3917682dc32f30cad09264f0cc70ce
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2025-04-07 15:53:15 +0100

    set defaults for TesTaskOptions.cs

--- app/BeaconBridge/Config/TesTaskOptions.cs
+++ app/BeaconBridge/Config/TesTaskOptions.cs
@@ -12,17 +12,17 @@ public class Outputs
   /// <summary>
   /// Gets or Sets Name
   /// </summary>
-  public string? Name { get; set; }
+  public string Name { get; set; } = string.Empty;
   
   /// <summary>
   /// URL in long term storage, for example: s3://my-object-store/file1 file:///path/to/my/file etc.
   /// </summary>
-  public string? Url { get; set; }
+  public string Url { get; set; } = string.Empty;
   
   /// <summary>
   /// Path of the file inside the container. Must be an absolute path.
   /// </summary>
-  public string? Path { get; set; }
+  public string Path { get; set; } = string.Empty;
 }
 
 public class BeaconImage
