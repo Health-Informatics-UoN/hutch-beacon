85f843648eab32d7552dcb80984d205349f4fda2
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2025-04-10 12:47:52 +0100

    add loading icon, change nav, redo buttons

--- app/beacon-bridge-ui/src/app/components/Header.js
+++ app/beacon-bridge-ui/src/app/components/Header.js
@@ -3,10 +3,19 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="bg-uon-gradient">
-      <span className={"flex items-baseline space-x-14"}>
-        <Image src={"/UoN-Nottingham-Blue-white-text-logo-RGB-300x112.png"} width={300} height={112} />
-        <h1 className={"text-6xl text-white"}>Beacon</h1>
-      </span>
+      <nav
+        className="mx-4 flex max-w-7xl items-center justify-between p-6 lg:px-1"
+        aria-label="Global"
+      >
+        <span className={"flex items-baseline space-x-5"}>
+          <Image
+            src={"/UoN-Nottingham-Blue-white-text-logo-RGB-300x112.png"}
+            width={200}
+            height={112}
+          />
+          <h1 className={"text-6xl text-white"}>Beacon</h1>
+        </span>
+      </nav>
     </div>
-  )
+  );
 }
--- app/beacon-bridge-ui/src/app/components/SearchDropdown.js
+++ app/beacon-bridge-ui/src/app/components/SearchDropdown.js
@@ -1,23 +1,25 @@
-export default function SearchDropdown({id, label, options, onChange}) {
+export default function SearchDropdown({ id, label, options, onChange }) {
   return (
-    <div className="flex flex-wrap space-x-2 mb-4 items-baseline">
+    <div className="flex flex-wrap mb-4 items-baseline">
       <label htmlFor={id}>{label}</label>
       <select
         id={id}
         onChange={(event) => {
-          onChange(event.target.value)}
-        }
+          onChange(event.target.value);
+        }}
         className="rounded-lg bg-transparent border-2 border-uon-blue-60 border-solid py-2 grow max-w-full"
         defaultValue={""}
       >
         <option value="" disabled></option>
-        {
-          options.map((opt, key)=>{
-            let name = `${opt.id} - ${opt.label}`;
-            return <option key={key} value={opt.id}>{name}</option>
-          })
-        }
+        {options.map((opt, key) => {
+          let name = `${opt.id} - ${opt.label}`;
+          return (
+            <option key={key} value={opt.id}>
+              {name}
+            </option>
+          );
+        })}
       </select>
     </div>
-  )
+  );
 }
